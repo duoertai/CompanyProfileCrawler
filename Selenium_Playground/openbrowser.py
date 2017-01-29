c0d8228c3a1a86923023c7dfa22d4c57ea6a328c
ertaiduo <duoertai@gmail.com>
2017-01-29 00:30:52 -0800

    tried out selenium

---
+++ Selenium_Playground/openbrowser.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+'''
+browser = webdriver.Chrome()
+browser.get('http://google.com')
+'''
+
+driver = webdriver.Chrome()
+driver.get('http://www.python.org')
+assert 'Python' in driver.title
+element = driver.find_element_by_name('q')
+element.send_keys('pycon')
+element.send_keys(Keys.RETURN)
+print driver.page_source
