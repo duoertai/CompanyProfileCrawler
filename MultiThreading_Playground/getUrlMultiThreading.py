efd64a691f617307f82f9b3de939a6ff70407c8a
ertaiduo <duoertai@gmail.com>
2017-01-31 01:23:45 -0800

    try to use multi-threading to fetch url

---
+++ MultiThreading_Playground/getUrlMultiThreading.py
@@ -0,0 +1,34 @@
+import Queue
+import threading
+import urllib2
+import time
+
+hosts = ["http://google.com", "http://yahoo.com", "http://apple.com"]
+
+queue = Queue.Queue()
+
+class ThreadUrl(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            host = self.queue.get()
+            url = urllib2.urlopen(host)
+            print url.read()
+            self.queue.task_done()
+
+start = time.time()
+
+for i in range(len(hosts)):
+    t = ThreadUrl(queue)
+    t.setDaemon(True)
+    t.start()
+
+for host in hosts:
+    queue.put(host)
+
+queue.join()
+
+print "Elapsed time: %s" % (time.time() - start)
